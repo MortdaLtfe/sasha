6775b6b954f783bdceea656cf830ac7a22783ca9
MortdaLtfe <mortda1039@gmail.com>
2024-03-25 13:00:25 +0300

    Add protect function to protect routes and check tokens

--- services/authService.js
+++ services/authService.js
@@ -5,7 +5,10 @@ import bcrypt from "bcryptjs";
 import ApiError from "../utils/apiError.js";
 
 const createToken = user => {
-  return jwt.sign({ uid: user._id }, process.env.SECRET_KEY);
+  return jwt.sign(
+    { userInfo: { uid: user._id, role: user.role } },
+    process.env.SECRET_KEY
+  );
 };
 /**
  * @desc    Create An Account in website
@@ -33,3 +36,29 @@ export const signIn = asyncHandler(async (req, res, next) => {
   const token = createToken(user);
   return res.json({ user, token });
 });
+/**
+ * @desc    Validate the Token and send it inside request
+ */
+export const protect = async (req, res, next) => {
+  if (!req.headers.authorization)
+    return res
+      .status(401)
+      .json({ message: "You're not authorized, Please Login 1" });
+
+  const token = req.headers.authorization.split(" ")[1];
+  
+  // 1)  Check if token is valid
+  const user = await jwt.verify(token, process.env.SECRET_KEY);
+
+  // 2) Check if the user inside the token is Active or avilable
+  const isAvilableUser = await User.findOne({ _id: user.userInfo.uid });
+  if (!isAvilableUser) {
+    return res
+      .status(401)
+      .json({ message: "You're not authorized, Please Login " });
+  }
+  // send the token through request
+  req.token = user;
+  console.log(user);
+  next();
+};
