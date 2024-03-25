d6ddff4499e4306def60cbaa9d765d6af28887b6
MortdaLtfe <mortda1039@gmail.com>
2024-03-25 13:06:42 +0300

    format

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,10 +5,19 @@ import {
   deleteUser,
   updateUser
 } from "../services/userService.js";
-import {getUserValidator, updateUserValidator, deleteUserValidator} from '../utils/validators/userServiceValidator.js'
+import {
+  getUserValidator,
+  updateUserValidator,
+  deleteUserValidator
+} from "../utils/validators/userServiceValidator.js";
+
 const router = express.Router();
 
 router.route("/").get(getUsers);
-router.route("/:user_id").get(getUserValidator,getUser).put(updateUserValidator,updateUser).delete(deleteUserValidator,deleteUser);
+router
+  .route("/:user_id")
+  .get(getUserValidator, getUser)
+  .put(updateUserValidator, updateUser)
+  .delete(deleteUserValidator, deleteUser);
 
 export default router;
